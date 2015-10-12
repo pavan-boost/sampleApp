266b93d8da948f337ac34ddc841b2d4f14113ec3
Pavan Kumar <pavan_boost@yahoo.com>
2015-10-11 22:39:58 -0700

    fixed a jshint issue

--- client/app/simpleGame/simpleGame.controller.js
+++ client/app/simpleGame/simpleGame.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('sampleAppApp')
-  .controller('SimpleGameCtrl', function ($scope) {
+  .controller('SimpleGameCtrl', function () {
     // Most of the code is in the  snake Game directive and snake service.
   });
